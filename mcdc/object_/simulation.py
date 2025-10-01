94cda33d68d4e69c54719c2a55753d6bdf4b6bad
Ilham Variansyah <variansi@oregonstate.edu>
2025-10-01 11:02:07 +0700

    complete typie-hinting

--- mcdc/object_/simulation.py
+++ mcdc/object_/simulation.py
@@ -1,5 +1,20 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from mcdc.object_.cell import Cell, Region
+    from mcdc.object_.material import MaterialBase
+    from mcdc.object_.surface import Surface
+    from mcdc.object_.tally import TallyBase
+
+####
+
 from mcdc.object_.base import ObjectSingleton
-from mcdc.object_.universe import Universe
+from mcdc.object_.data import DataBase
+from mcdc.object_.distribution import DistributionBase
+from mcdc.object_.mesh import MeshBase
+from mcdc.object_.nuclide import Nuclide
+from mcdc.object_.reaction import ReactionBase
+from mcdc.object_.universe import Universe, Lattice
 from mcdc.object_.settings import Settings
 
 
@@ -9,6 +24,21 @@ from mcdc.object_.settings import Settings
 
 
 class Simulation(ObjectSingleton):
+    # Annotations for Numba mode
+    data: list[DataBase]
+    distributions: list[DistributionBase]
+    materials: list[MaterialBase]
+    nuclides: list[Nuclide]
+    reactions: list[ReactionBase]
+    cells: list[Cell]
+    lattices: list[Lattice]
+    regions: list[Region]
+    surfaces: list[Surface]
+    universes: list[Universe]
+    meshes: list[MeshBase]
+    settings: Settings
+    tallies: list[TallyBase]
+
     def __init__(self):
         label = "simulation"
         super().__init__(label)
