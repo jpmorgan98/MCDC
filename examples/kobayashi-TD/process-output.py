38c55951dfb1365079f207f522d19fe5d60e4acc
Ilham Variansyah <variansi@oregonstate.edu>
2025-10-31 13:22:10 +0700

    update example

--- examples/kobayashi-TD/process-output.py
+++ examples/kobayashi-TD/process-output.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 import h5py
 import matplotlib.animation as animation
 
-# =============================================================================
-# Plot results
-# =============================================================================
-
-# Results
+# Load result
 with h5py.File("output.h5", "r") as f:
     x = f["tallies/mesh_tally_0/grid/x"][:]
     x_mid = 0.5 * (x[:-1] + x[1:])
@@ -19,20 +15,18 @@ with h5py.File("output.h5", "r") as f:
 
     phi = f["tallies/mesh_tally_0/flux/mean"][:]
     phi_sd = f["tallies/mesh_tally_0/flux/sdev"][:]
+    
+    phi_total = f["tallies/global_tally_0/density/mean"][:]
+    phi_total_sd = f["tallies/global_tally_0/density/sdev"][:]
 
-phi = np.sum(phi, axis=3)
-phi_sd = np.linalg.norm(phi_sd, axis=3)
-
-phi_total = np.sum(phi, axis=(1, 2))
-phi_total_sd = np.linalg.norm(phi_sd, axis=(1, 2))
-
+# Animate result
 fig, ax = plt.subplots(1, 2, figsize=(8, 4), gridspec_kw={"width_ratios": [1.0, 2]})
-
+#
 cax = ax[1].pcolormesh(X, Y, phi[0], vmin=phi[0].min(), vmax=phi[0].max())
 ax[1].set_aspect("equal", "box")
 ax[1].set_xlabel("$y$ [cm]")
 ax[1].set_ylabel("$x$ [cm]")
-
+#
 ax[0].plot(t_mid, phi_total)
 ax[0].set_xlabel("$t$ [s]")
 ax[0].set_ylabel("Neutron density")
@@ -44,16 +38,14 @@ ax[0].fill_between(
 ax[0].grid()
 ax[0].set_box_aspect(1)
 (line,) = ax[0].plot([], [], "ok", fillstyle="none")
-
-
+#
 def animate(i):
     n = np.zeros_like(t_mid)
     n[i] = phi_total[i]
     line.set_data(t_mid, n)
     cax.set_array(phi[i])
     cax.set_clim(phi[i].min(), phi[i].max())
-
-
+#
 K = len(t) - 1
 anim = animation.FuncAnimation(fig, animate, frames=K)
 plt.show()
